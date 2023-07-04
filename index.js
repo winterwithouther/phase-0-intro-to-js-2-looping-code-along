3ac3a9b339f6a3f8e67327fedd9a79584dced50a
Eman Gurung <egurung1117@gmail.com>
2023-07-04 05:35:52 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards(obj, event) {
+    const newObj = [];
+    for (let i = 0; i < obj.length; i++) {
+        newObj.push(`Thank you, ${obj[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return newObj;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
